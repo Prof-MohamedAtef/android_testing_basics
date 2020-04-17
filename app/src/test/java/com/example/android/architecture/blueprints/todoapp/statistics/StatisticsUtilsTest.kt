65857e6742528309e64ce40109265d94428444dd
Prof-Mohamed Atef <proofesser@gmail.com>
2020-04-17 15:23:36 +0200

    local test - JVM tests

---
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
@@ -0,0 +1,26 @@
+package com.example.android.architecture.blueprints.todoapp.statistics
+
+import com.example.android.architecture.blueprints.todoapp.data.Task
+import org.junit.Assert.*
+import org.junit.Test
+
+class StatisticsUtilsTest{
+
+    @Test
+    fun getActiveAndCompletedStats_noCompleted_returnsHundredZero(){
+        // Create an active task
+
+        val tasks= listOf<Task>(
+                Task("title","desc",isCompleted = false)
+        )
+
+        val result= getActiveAndCompletedStats(tasks)
+
+        assertEquals(result.completedTasksPercent,0f)
+        assertEquals(result.activeTasksPercent, 100f)
+
+        // Call your function
+
+        // Check the result
+    }
+}
