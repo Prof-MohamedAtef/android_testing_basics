54feef44d9391f4f653e0869748fd5b8ed2fbb26
Prof-Mohamed Atef <proofesser@gmail.com>
2020-04-18 15:47:14 +0200

    observe in the same viewModel

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -1,9 +1,15 @@
 package com.example.android.architecture.blueprints.todoapp.tasks
 
 import android.os.Build
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.android.architecture.blueprints.todoapp.Event
+import org.hamcrest.core.IsNot.not
+import org.hamcrest.core.IsNull.nullValue
 import org.junit.Assert.*
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.annotation.Config
@@ -12,9 +18,30 @@ import org.robolectric.annotation.Config
 @Config(sdk = [Build.VERSION_CODES.O_MR1])
 class TasksViewModelTest{
 
+    @get:Rule
+    var instantExecutorRule=InstantTaskExecutorRule()
+
     @Test
     fun addNewTask_setsNewTaskEvent(){
         val tasksViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
+
+        val observer = Observer<Event<Unit>> {}
+
+        try {
+// Observe the LiveData forever
+            tasksViewModel.newTaskEvent.observeForever(observer)
+
+            // When adding a new task
+            tasksViewModel.addNewTask()
+
+            // Then the new task event is triggered
+            val value = tasksViewModel.newTaskEvent.value
+            assertThat(value?.getContentIfNotHandled(), (not(nullValue())))
+        }finally {
+            // Whatever happens, don't forget to remove the observer!
+            tasksViewModel.newTaskEvent.removeObserver(observer)
+        }
+
         tasksViewModel.addNewTask()
     }
 }
