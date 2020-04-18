811950dd505adc601be05494ada44b73468fa522
Prof-Mohamed Atef <proofesser@gmail.com>
2020-04-18 13:50:44 +0200

    use Robolectric library for Android Emulator Simulation(faster operation) & see Manifest file

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -1,12 +1,15 @@
 package com.example.android.architecture.blueprints.todoapp.tasks
 
+import android.os.Build
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Assert.*
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.robolectric.annotation.Config
 
 @RunWith(AndroidJUnit4::class)
+@Config(sdk = [Build.VERSION_CODES.O_MR1])
 class TasksViewModelTest{
 
     @Test
